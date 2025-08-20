6dc79b053a02856f90e30dd7cfa9e12bc4efee36
Edwin Game Dev <edwinjonesholanda@gmail.com>
2025-08-20 11:55:09 +0100

    Add binary tree + binary search project

---
+++ BinaryTree/sketch.js
@@ -0,0 +1,32 @@
+var tree;
+var numbers = [];
+var numbersString="";
+
+function setup() {
+  createCanvas(640, 360);
+  background(30);
+
+  tree = new Tree();
+
+  for (var i = 0; i < 20; i++) {
+    let number = floor(random(0, 100));
+    numbersString += number+', ';
+    tree.addValue(number);
+  }
+
+  numbersString
+
+  bottomSection();
+
+  tree.traverse();
+}
+
+bottomSection = function () {
+  fill(255,100,125);
+  rect(0, height - 30, width, 30);
+  fill(255);
+  noStroke();
+  textAlign(CENTER);
+
+  text(numbersString,  width / 2, height - 10);
+};
